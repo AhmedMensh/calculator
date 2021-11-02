7a38553f7115f305dd7a66cd808a668011c6eddd
AhmedMensh <ahmedmensh8@gmail.com>
2021-11-02 19:11:54 +0200

    first commmit

---
+++ app/src/main/java/com/example/calculator/cal/Calculator.kt
@@ -0,0 +1,9 @@
+package com.example.calculator.cal
+
+class Calculator {
+
+    fun add(a : Int ,b : Int) = a+b
+    fun sub(a : Int ,b : Int) = a-b
+    fun divide(a : Int ,b : Int) = a/b
+    fun multiply(a : Int ,b : Int) = a*b
+}
